256c7632d54d15bac9567cf04393ab026ecf11b4
snadi043 <snadi043@uottawa.ca>
2024-11-18 15:30:34 -0500

    Changed the ChatScreen Widget from Stateless to Stateful and instantiated the FirebaseMessaging package to display permission to allow push notofocations for the users who already had logged in atlease once and collecting the token.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,31 @@
 import 'package:chat_app/widgets/chat_message.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:chat_app/widgets/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() {
+    return _ChatScreen();
+  }
+}
+
+class _ChatScreen extends State<ChatScreen> {
+  void pushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    final token = await fcm.getToken();
+    print(token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
